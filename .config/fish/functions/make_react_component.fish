735e467d42097d2398116718fdec8cd3ce380a85
sfurukawa7 <shota.trp.musik711@gmail.com>
2022-11-06 13:27:28 +0900

    mkcmpコマンド修正

--- .config/fish/functions/make_react_component.fish
+++ .config/fish/functions/make_react_component.fish
@@ -6,6 +6,7 @@ function make_react_component
     cd ./$argv
 
     echo "type SampleProps = {"\n"};"\n\n"const Sample = (props:SampleProps) => {"\n"return <></>;"\n"};"\n\n"export default Sample;" > index.tsx
+    gsed -i s/Sample/$argv/g index.tsx
     echo "import { StyleSheet } from 'react-native';"\n\n"export const styles = StyleSheet.create({"\n"});" > styles.ts
     touch hooks.tsx
   end
