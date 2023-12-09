65034b690ac2de0f89868452ec96868136d29d68
sfurukawa7 <shota.trp.musik711@gmail.com>
2023-12-09 18:15:31 +0900

    change fish function to insert a title into a new quick note

--- .config/fish/functions/obsidian_quick_note.fish
+++ .config/fish/functions/obsidian_quick_note.fish
@@ -2,6 +2,7 @@ function obsidian_quick_note
   if test "$argv" = ""
     nvim /Users/sfurukawa/Obsidian/QuickNote/(date "+%Y%m%d_%H%M%S")\.md
   else
+    echo "# $argv" > /Users/sfurukawa/Obsidian/QuickNote/{$argv}\.md
     nvim /Users/sfurukawa/Obsidian/QuickNote/{$argv}\.md
   end
 end
