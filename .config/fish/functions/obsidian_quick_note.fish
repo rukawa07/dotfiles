8e3ca2462d01fc783fda5a0f200d3992cdf0fc5f
sfurukawa7 <shota.trp.musik711@gmail.com>
2024-05-01 17:47:28 +0900

    obsidian quicknoteの修正

--- .config/fish/functions/obsidian_quick_note.fish
+++ .config/fish/functions/obsidian_quick_note.fish
@@ -1,8 +1,8 @@
 function obsidian_quick_note
   if test "$argv" = ""
-    nvim /Users/sfurukawa/Obsidian/QuickNote/(date "+%Y%m%d_%H%M%S")\.md
+    nvim /Users/sfurukawa/Obsidian/MyVault/QuickNotes/(date "+%Y%m%d_%H%M%S")\.md
   else
-    echo "# $argv" > /Users/sfurukawa/Obsidian/QuickNote/{$argv}\.md
-    nvim /Users/sfurukawa/Obsidian/QuickNote/{$argv}\.md
+    echo "# $argv" > /Users/sfurukawa/Obsidian/MyVault/QuickNotes/{$argv}\.md
+    nvim /Users/sfurukawa/Obsidian/MyVault/QuickNotes/{$argv}\.md
   end
 end
