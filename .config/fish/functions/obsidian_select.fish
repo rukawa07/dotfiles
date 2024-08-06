597a87d48c24b269900fb179f08901466c4210b0
sfurukawa7 <shota.trp.musik711@gmail.com>
2024-08-06 23:17:56 +0900

    obsコマンドにpreviewを表示させるように変更

--- .config/fish/functions/obsidian_select.fish
+++ .config/fish/functions/obsidian_select.fish
@@ -1,5 +1,5 @@
 function obsidian_select
-  set foo (find ~/Obsidian/ -name '*.md' | fzf-tmux -p --layout reverse)
+  set foo (find ~/Obsidian/ -name '*.md' | fzf-tmux -p 75% --layout reverse --preview 'bat --color=always {}')
   if test "$foo" != ""
     nvim "$foo"
   else
