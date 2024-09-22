e1767cddcf968ecc6aefebfd4d792914c3cd02bd
sfurukawa7 <shota.trp.musik711@gmail.com>
2024-09-22 22:48:27 +0900

    zによるディレクトリ移動のバグを解消

--- .config/fish/functions/select_dir_in_z.fish
+++ .config/fish/functions/select_dir_in_z.fish
@@ -1,6 +1,6 @@
 function select_dir_in_z
   if type -q z
-    z -l|fzf --layout reverse|string replace -r '[0-9]+\s*' ''|read foo
+    z -l | fzf --layout reverse | string replace -r '[0-9]+(\.[0-9]+)?\s*' '' | read foo
 
     if test -d $foo
       builtin cd $foo
