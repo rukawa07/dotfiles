db92512e8ff19e65013371c4c7c91de7b206decb
sfurukawa7 <shota.trp.musik711@gmail.com>
2024-09-25 22:06:43 +0900

    qnoteからnoteにリネーム＆上書き防止

--- .config/fish/functions/obsidian_quick_note.fish
+++ .config/fish/functions/obsidian_quick_note.fish
@@ -1,6 +1,8 @@
 function obsidian_quick_note
   if test "$argv" = ""
     nvim /Users/sfurukawa/Obsidian/MyVault/QuickNotes/(date "+%Y%m%d_%H%M%S")\.md
+  else if test -e /Users/sfurukawa/Obsidian/MyVault/QuickNotes/{$argv}\.md
+    nvim /Users/sfurukawa/Obsidian/MyVault/QuickNotes/{$argv}\.md
   else
     echo "# $argv" > /Users/sfurukawa/Obsidian/MyVault/QuickNotes/{$argv}\.md
     nvim /Users/sfurukawa/Obsidian/MyVault/QuickNotes/{$argv}\.md
