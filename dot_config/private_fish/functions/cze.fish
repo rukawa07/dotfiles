b2ed344fdd7d8caf91b0ac4d0b3455c879368fa2
sfurukawa7 <shota.trp.musik711@gmail.com>
2026-06-20 18:53:05 +0900

    czeでファイルのみ候補に表示するように変更

--- dot_config/private_fish/functions/cze.fish
+++ dot_config/private_fish/functions/cze.fish
@@ -1,5 +1,5 @@
 function cze --description 'chezmoi管理ファイルをfzfで選んで編集'
-    set foo (chezmoi managed --path-style absolute | fzf-tmux -p 75% --layout reverse --preview 'bat --color=always {}')
+    set foo (chezmoi managed --path-style absolute --include=files | fzf-tmux -p 75% --layout reverse --preview 'bat --color=always {}')
     if test "$foo" != ""
         chezmoi edit "$foo"
     else
