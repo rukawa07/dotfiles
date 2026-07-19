e470936df933750b5a7e28d637ed309c6d269d52
sfurukawa7 <shota.trp.musik711@gmail.com>
2026-07-19 14:49:10 +0900

    fleetingノートの設定を変更

--- dot_config/private_fish/functions/obsidian_fleeting.fish
+++ dot_config/private_fish/functions/obsidian_fleeting.fish
@@ -1,11 +1,12 @@
 function obsidian_fleeting
   set -l timestamp (date "+%Y-%m-%dT%H:%M:%S")
   set -l filename
+  set -l obsidian_path $HOME"/Workspace/private/obsidian/Main"
   
   if test "$argv" = ""
-    set filename ~/Obsidian/MyVault/Fleeting/(date "+%Y%m%d_%H%M%S")\.md
+    set filename $obsidian_path/Fleeting/(date "+%Y%m%d_%H%M%S")\.md
   else
-    set filename ~/Obsidian/MyVault/Fleeting/{$argv}\.md
+    set filename $obsidian_path/Fleeting/{$argv}\.md
   end
   
   # frontmatterを含むテンプレートを書き込む
